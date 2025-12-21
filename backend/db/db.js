aa252b39c018d3f95fd4ffd5906ac4a451ece204
anurag <yourboyanurag777@gmail.com>
2025-12-21 17:07:29 +0530

    Added db connection

---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+}catch(error){
+    console.log(error);
+    process.exit(1);
+    res.status.json({message:"error"})
+}
+}
